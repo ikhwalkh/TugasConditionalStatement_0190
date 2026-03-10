088e7019e07bd11624ee60c2e36d6923f7b13c2f
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-03-10 09:30:19 +0700

    mengganti nama file

---
+++ tugas praktikum yang disuruh.cpp
@@ -0,0 +1,53 @@
+//Library
+#include <iostream>
+using namespace std;
+
+// memasukkan input data nya
+void inputData(float &berat, float &tinggi)
+{
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
+
+// Rumus BMI nya
+float hitungBMI(float berat, float tinggi)
+{
+    float bmi;
+    bmi = berat / (tinggi * tinggi);
+    return bmi;
+}
+
+// untuk menentukan kondisi menggunakan if else di gunakan untu mengambil keputusaan secara otomatis
+string cekKondisi(float bmi)
+{
+    if (bmi < 18.5)
+        return "Berat Badan Kurang";
+    else if (bmi < 25) // else if adalah sebuah kondisi tambahan jika kondisi if salah maka akan di lanjutkan dengan menggunakan else if
+        return "Berat Badan Normal";
+    else if (bmi < 30)
+        return "Berat Badan Kelebihan";
+    else
+        return "Obesitas";
+}
+
+//untuk menjalankan program yang telah dibuat 
+int main()
+{
+    float berat, tinggi, bmi;
+
+    cout << "--- Selamat Datang Mahasiswa Kelas D 2025 ---" << endl; //di dapatkan dari hasil menyontek code sebelumnya
+
+    inputData(berat, tinggi);
+
+    bmi = hitungBMI(berat, tinggi);
+    
+    cout << endl;
+    cout << "--- hasil ---" << endl;
+    cout << "Nilai BMI: " << bmi << endl;
+    cout << "Kondisi: " << cekKondisi(bmi) << endl;
+
+    return 0;      //tinggal run code
+}
